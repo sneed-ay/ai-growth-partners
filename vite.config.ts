f6950ef15e1d1fabc088269dcc923acc4adb730a
yusuke ito <yusukeito@yusukenoMacBook-Pro.local>
2025-07-23 00:39:39 +0900

    Add GitHub Actions for automatic deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react'; // この行を追加
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: "/ai-growth-partners/", // この行を追加！
+      plugins: [react()], // この行を追加！
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
